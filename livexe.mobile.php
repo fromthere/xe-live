a86593f13442e363eb83e37dfbfe6ebddd63ee16
flourscent <flourscent@dbef5163-b189-ecad-0cd8-6ae99ca8dcce>
2011-03-16 08:13:43 +0000

    들여쓰기를 스페이스에서 탭으로 교체

--- livexe.mobile.php
+++ livexe.mobile.php
@@ -8,27 +8,27 @@
 		function init()
 		{
 			// 모바일 스킨 설정
-            $template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
-            if(!is_dir($template_path)||!$this->module_info->mskin) {
-                $this->module_info->mskin = 'default';
-                $template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
-            }
-            $this->setTemplatePath($template_path);
+			$template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
+			if(!is_dir($template_path)||!$this->module_info->mskin) {
+				$this->module_info->mskin = 'default';
+				$template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
+			}
+			$this->setTemplatePath($template_path);
 
-            $args->module_srl = $this->module_srl;
-            $output = executeQuery('livexe.getRSSCount', $args);
-            Context::set('total_feeds', $output->data->count);
+			$args->module_srl = $this->module_srl;
+			$output = executeQuery('livexe.getRSSCount', $args);
+			Context::set('total_feeds', $output->data->count);
 
-            $output = executeQuery('livexe.getDocumentCount', $args);
-            Context::set('total_articles', $output->data->count);
+			$output = executeQuery('livexe.getDocumentCount', $args);
+			Context::set('total_articles', $output->data->count);
 
-            if(Context::get('is_logged')) {
-                $logged_info = Context::get('logged_info');
-                $args->module_srl = $this->module_srl;
-                $args->member_srl = $logged_info->member_srl;
-                $output = executeQuery('livexe.getMyRSSCount', $args);
-                Context::set('total_my_feeds', $output->data->count);
-            }
+			if(Context::get('is_logged')) {
+				$logged_info = Context::get('logged_info');
+				$args->module_srl = $this->module_srl;
+				$args->member_srl = $logged_info->member_srl;
+				$output = executeQuery('livexe.getMyRSSCount', $args);
+				Context::set('total_my_feeds', $output->data->count);
+			}
 		}
 
 		function dispLivexeContent() {
@@ -48,15 +48,15 @@
 
 		function dispLivexeCrawler()
 		{
-            $oLivexeController = &getController('livexe');
+			$oLivexeController = &getController('livexe');
 
-            $status = $oLivexeController->doCrawl();
+			$status = $oLivexeController->doCrawl();
 
-            $status_args->module_srl = $this->module_info->module_srl;
-            $output = executeQuery('livexe.getRSSCount', $status_args);
-            $status['total_rss'] = $output->data->count;
+			$status_args->module_srl = $this->module_info->module_srl;
+			$output = executeQuery('livexe.getRSSCount', $status_args);
+			$status['total_rss'] = $output->data->count;
 
-            Context::set('status', $status);
+			Context::set('status', $status);
 			$this->setTemplateFile('crawler');
 		}
 
